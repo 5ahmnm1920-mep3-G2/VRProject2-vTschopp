9b2895c6db6fb4db5e1ce85d9e81cbf29332fd8f
NotMyTschopp <45820353+NotMyTschopp@users.noreply.github.com>
2020-02-05 09:22:19 +0100

    Add rotating globe

---
+++ Assets/MyGame/Scripts/RotateGlobe.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class RotateGlobe : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField, Range(-10f, 10f)] float rotationSpeed;
+
+    void Update()
+    {
+        transform.Rotate(0, rotationSpeed, 0, Space.World);
+    }
+}
